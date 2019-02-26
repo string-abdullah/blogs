d2e76487a08641a2e19fa4e1ecb705a4e6e30be9
string-abdullah <abdullah.khan@string.org.in>
2019-02-26 18:33:43 +0530

    third commit with model and database

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'sarwarclasses',
+        'PASSWORD':'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,41 @@
 from __future__ import unicode_literals
+from django.contrib.auth.models import User
 
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+	title=models.CharField(max_length=120)
+	content=models.TextField()
+	post_pic=models.ImageField(null=True,blank=True)
+	timestamp=models.DateField(auto_now=False,auto_now_add=True)
+	
+
+
+	def __unicode__(self):
+		return self.title
+
+	def __str__(self):
+		return self.title
+
+
+class Comment(models.Model):
+	post=models.ForeignKey(Post,on_delete=models.CASCADE)
+	user=models.ForeignKey(User,on_delete=models.CASCADE)
+	comment=models.CharField(max_length=300)
+	timestamp=models.DateField(auto_now=False,auto_now_add=True)
+
+
+	def __unicode__(self):
+		return self.title
+
+	def __str__(self):
+		return self.title
+
+
+
+
+
+
+
